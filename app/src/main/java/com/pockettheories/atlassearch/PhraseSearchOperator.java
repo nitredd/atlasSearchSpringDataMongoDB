e9c28b84e5a54d31db7452c4e5d5b753415205cc
Nitin Reddy <82951937+nitredd@users.noreply.github.com>
2022-06-24 21:19:46 +0400

    Support for multiple search queries

--- app/src/main/java/com/pockettheories/atlassearch/PhraseSearchOperator.java
+++ app/src/main/java/com/pockettheories/atlassearch/PhraseSearchOperator.java
@@ -3,16 +3,19 @@ package com.pockettheories.atlassearch;
 import org.bson.Document;
 
 import javax.print.Doc;
+import java.util.List;
 
 public class PhraseSearchOperator implements SearchOperator {
-    String query;
-    String path;
+    Object query = null;
+    String path = null;
     Integer fuzzyMaxEdits = null;
 
 
     public PhraseSearchOperator(String query, String path, Integer fuzzyMaxEdits) { this.query = query; this.path = path; this.fuzzyMaxEdits = fuzzyMaxEdits; }
     public PhraseSearchOperator(String query, String path) { this(query, path, null); }
-    public PhraseSearchOperator() { this(null, null, null); }
+    public PhraseSearchOperator(List<String> query, String path, Integer fuzzyMaxEdits) { this.query = query; this.path = path; this.fuzzyMaxEdits = fuzzyMaxEdits; }
+    public PhraseSearchOperator(List<String> query, String path) { this(query, path, null); }
+    public PhraseSearchOperator() { /* this(null, null, null); */ }
 
 
     @Override
